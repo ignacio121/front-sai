a56de52a8359d9157d2268353637b888607a8919
Ignacio121 <ignaciomunoz121@gmail.com>
2024-05-11 04:51:02 -0400

    Se prueban peticiones para GitPages

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
 
   const [state1, setState1] = useState(false);
+
+
+  const get = async ()=> {
+    setState1(true);
+    const res = await axios.get("https://localhost:3000");
+  }
   
   return (
     <div className="App">
       <h1>
         Hola buenas
       </h1>
-      <button onClick={()=>setState1(true)}>
+      <button onClick={()=>get()}>
         Click
       </button>
       {state1===true &&(
