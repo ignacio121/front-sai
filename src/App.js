bbe026bf2335414aaebcd23e5f4d9aacc642c7fe
Ignacio121 <ignaciomunoz121@gmail.com>
2024-05-10 20:13:29 -0400

    Probando funcionalidades de React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [state1, setState1] = useState(false);
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+        Hola buenas
+      </h1>
+      <button onClick={()=>setState1(true)}>
+        Click
+      </button>
+      {state1===true &&(
+        <h1>
+          Funco?
+        </h1>
+      )}
     </div>
   );
 }
