8511901866696000404d3bb5e2a555f0db2d1d0d
cristofer <cristofer.mondaca@alu.ucm.cl>
2024-06-13 23:30:06 -0400

    cambios en option con los datos recopilados y preguntas frecuentes

--- src/pages/estudiante.jsx
+++ src/pages/estudiante.jsx
@@ -7,7 +7,7 @@ import Options from '../components/options';
 import Anuncios from '../components/anuncios';
 import { getCategorias } from '../Redux/actions/categoriaActions';
 import { getDestinatarios } from '../Redux/actions/destinatarioActions';
-import {fetchPreguntasFrecuentes} from '../Redux/actions/pfActions';
+import { fetchPreguntasFrecuentes } from '../Redux/actions/pfActions';
 
 function EstudiantePage() {
   const navigate = useNavigate();
@@ -15,6 +15,7 @@ function EstudiantePage() {
   const { user, token, loading, error } = useSelector((state) => state.auth);
   const { categorias } = useSelector((state) => state.categorias);
   const { destinatarios } = useSelector((state) => state.destinatarios);
+  const { preguntasFrecuentes } = useSelector((state) => state.preguntasFrecuentes);
 
   useEffect(() => {
     if (!token) {
@@ -41,19 +42,20 @@ function EstudiantePage() {
   useEffect(() => {
     if (user) {
       console.log('Usuario:', user);
-      console.log('userId:', user.userId);
-      console.log('carrera_id:', user.carrera_id);
+      console.log('userId:', user.userId); // Acceder al userId desde user
+      console.log('carrera_id:', user.carrera_id); // Acceder a carrera_id desde user
     }
   }, [user]);
-  const { preguntasFrecuentes } = useSelector((state) => state.preguntasFrecuentes);
 
-useEffect(() => {
-  if (preguntasFrecuentes) {
-    console.log('Preguntas Frecuentes:', preguntasFrecuentes);
-    // Aquí podrías manejar las preguntas frecuentes y mostrarlas en tu interfaz
-  }
-}, [preguntasFrecuentes]);
-  
+  useEffect(() => {
+    if (preguntasFrecuentes) {
+      console.log('Preguntas Frecuentes:', preguntasFrecuentes);
+      // Aquí podrías manejar las preguntas frecuentes y mostrarlas en tu interfaz
+    }
+  }, [preguntasFrecuentes]);
+  const loginResponse = JSON.parse(localStorage.getItem('loginResponse'));
+  console.log("response",loginResponse)
+
   return (
     <div>
       {token ? (
@@ -69,8 +71,8 @@ useEffect(() => {
                 email={user?.email}
                 foto={user?.foto}
                 carrera={user?.carrera?.nombre}
-                userId={user?.userId}  
-                carreraId={user?.carrera_id}  
+                userId={user?.userId}  // Pasar userId desde user
+                carreraId={user?.carrera_id}  // Pasar carrera_id desde user
               />
               <Options 
                 categorias={categorias}
